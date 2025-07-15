d109599d8975b482cea0203a33b5e09d89cf426f
datascience134 <datasciecen134@gmail.com>
2025-07-15 16:45:56 +0800

    Testing api key loading on streamlit

--- app.py
+++ app.py
@@ -6,7 +6,22 @@
 import base64
 import streamlit as st
 
+from openai import AzureOpenAI
+
 st.title("Vision-Based Entity Extractor")
+
+
+AZUREOPENAI_ENDPOINT = st.secrets["AZUREOPENAI_ENDPOINT"]
+AZUREOPENAI_API_KEY = st.secrets["AZUREOPENAI_API_KEY"]
+
+st.write("Loaded endpoint and API key successfully!")
+
+client = AzureOpenAI(
+  azure_endpoint = AZUREOPENAI_ENDPOINT,
+  api_key=AZUREOPENAI_API_KEY,
+  api_version="2024-02-01"
+)
+
 uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if uploaded_file:
     st.image(uploaded_file)
