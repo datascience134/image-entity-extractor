d7ae3ca1f4397dde360cec73d4d07fe226ffb168
datascience134 <datasciecen134@gmail.com>
2025-07-15 16:31:54 +0800

    Convert image to base64

--- app.py
+++ app.py
@@ -3,12 +3,14 @@
 
 # In[1]:
 
-
+import base64
 import streamlit as st
 
 st.title("Vision-Based Entity Extractor")
 uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if uploaded_file:
     st.image(uploaded_file)
-    st.success("Now run inference here...")
+    image_bytes = uploaded_file.read()
+    img_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    st.success("Image successfully converted to base64!")
 
